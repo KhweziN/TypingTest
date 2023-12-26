9bae808391eb501bc8754ae7b643a54be8c52dd9
Khwezi N <khwezisandi@gmail.com>
2023-12-26 16:17:49 +0200

    Changed index.css to make use of variables during animation

--- javascript/index.js
+++ javascript/index.js
@@ -4,31 +4,16 @@ document.addEventListener("keypress", event =>{
     if (event.code === "Space"){
         console.log("HIT");
         textAreaElement.classList.add("translate-animate");
-
-        // textAreaElement.animate(
-        //     //keyframes
-        //     [
-        //         {transform: "translateY(0px)"},
-        //         {transform: "translateY(-53px)"}
-        //     ],
-
-        //     //timing options
-        //     {
-        //         duration: 0.5,
-        //         fill: "forwards",
-        //         iterations: 1
-        //     }
-        // );
-        // document.addEventListener("finish", () => {
-        //     textAreaElement.classList.remove("translate-animate");
-        // });
     }
 });
 
-textAreaElement.addEventListener('animationend', () => {
+textAreaElement.addEventListener('animationend', (animationEvent) => {
     textAreaElement.classList.remove("translate-animate");
-    var typingAreaStyle = window.getComputedStyle(textAreaElement, null);
-    var topValue = typingAreaStyle.getPropertyValue("top").replace("px","");
-    textAreaElement.style.top = (Number(topValue) - 53) + "px";
-    console.log((Number(topValue) - 5) + "px");
+    let typingAreaStyle = window.getComputedStyle(textAreaElement, null);
+
+    if(animationEvent.animationName === "translate-area-up"){
+        var topValue = typingAreaStyle.getPropertyValue("top").replace("px","");
+        textAreaElement.style.top = (Number(topValue) - 53) + "px";
+    }
+
 });
